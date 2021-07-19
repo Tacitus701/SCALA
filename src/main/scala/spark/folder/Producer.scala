ba4f6a1fcb7c2e4a70b0dea05e907cff9f9882ff
JcAntoine <jcantoine@live.fr>
2021-07-19 15:09:04 +0200

    Add gitignore and producer

--- src/main/scala/spark/folder/Producer.scala
+++ src/main/scala/spark/folder/Producer.scala
@@ -2,17 +2,19 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
 
-val props: Properties = new Properties()
-props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:9092")
-props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+object Producer {
+  val props: Properties = new Properties()
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:9092")
+  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
 
-val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+  val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
 
-val key = "akey"
-val value = "adata"
+  val key = "akey"
+  val value = "adata"
 
-val record = new ProducerRecord[String, String]("atopic", key, value)
-producer.send(record)
+  val record = new ProducerRecord[String, String]("atopic", key, value)
+  producer.send(record)
 
-producer.close()
+  producer.close()
+}

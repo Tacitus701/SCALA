49bba451dcafff8accd4ae5da9b9f5fc24b595ac
Intersideral <37081766+Intersideral@users.noreply.github.com>
2021-07-20 18:14:15 +0200

    Analyze part with questions
    Still need to be able to read from distributed FS

---
+++ src/main/scala/spark/folder/Analyze.scala
@@ -0,0 +1,42 @@
+package spark.folder
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecords, KafkaConsumer}
+
+import java.util.Properties
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import java.time.LocalDate
+import scala.collection.JavaConverters._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.rdd.RDD
+import Producer.Message
+
+object Analyze {
+
+  def main(args: Array[String]): Unit = {
+		val session = PeaceSparkSession.sparkSession()
+    val alerts = session.read.parquet("adl://peaceland.azuredatalakestore.net/Record")
+		val n = alerts.count()
+		alerts.show()
+  }
+
+  /*def answer1(alerts: RDD[Message], count: Long) = {
+		val r = alerts.map((e) => e.score).reduce(_ + _) / count
+		println("The mean score is : ${r} .")
+  }*/
+
+	def answer2(alerts: RDD[Message]) = {
+		val r = alerts.map(e => e.name).filter(e => e.startsWith("Dr.")).count()
+		println("There are ${r} doctors in the alerts.")
+  }
+	
+	def answer3(alerts: RDD[Message]) = {
+		val r = alerts.map(e => e.pos._1).filter(e => e > 0).count()
+		println("There are ${r} reports in the northern hemisphere.")
+  }
+	
+	def answer4(alerts: RDD[Message]) = {
+		val r = alerts.map(e => e.date).filter(e => e.isEqual(LocalDate.now())).count()
+		println("There are ${r} reports today.")
+  }
+}
